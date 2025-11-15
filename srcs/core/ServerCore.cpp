5c450edd7f1018ef567eeed8b6cd2cdc4357fe73
houdaifa drahm <houdaifadrahm@gmail.com>
2025-11-15 19:10:58 +0100

    nov15 after minor f update on init_sockets

--- srcs/core/ServerCore.cpp
+++ srcs/core/ServerCore.cpp
@@ -4,41 +4,59 @@ ServerCore::ServerCore(const Config &config) : config(config) {}
 
 void ServerCore::init_sockets()
 {
+    std::map<int, bool> has_wildcard;
+    std::set<std::pair<std::string,int> > skipped;
+
+    for (size_t i = 0; i < config.pairs.size(); i++)
+    {
+        const std::string &ip = config.pairs[i].first;
+        int port = config.pairs[i].second;
+        if (ip == "0.0.0.0")
+            has_wildcard[port] = true;
+    }
+
     int socket_fd;
     for (size_t i = 0; i < config.pairs.size(); i++)
     {
-        if (listen_sockets.find(config.pairs[i]) == listen_sockets.end())
+        const std::string &ip = config.pairs[i].first;
+        int port = config.pairs[i].second;
+
+        if (listen_sockets.find(config.pairs[i]) != listen_sockets.end())
+            throw CoreError("duplicated", 1, "ports");
+
+        if (skipped.find(config.pairs[i]) != skipped.end())
+            throw CoreError("duplicated", 1, "ports");
+
+        if (has_wildcard[port] && ip != "0.0.0.0")
         {
-            socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-            if (socket_fd < 0)
-            {
-                throw CoreError("socket()", errno, strerror(errno));
-            }
-            int opt_val = 1;
-            if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof(opt_val)) < 0)
-            {
-                throw CoreError("setsockopt()", errno, strerror(errno));
-            }
-            struct sockaddr_in addr;
-            addr.sin_family = AF_INET;
-            addr.sin_port = htons(config.pairs[i].second);
-            addr.sin_addr.s_addr = inet_addr(config.pairs[i].first.c_str());
-            if (bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-            {
-                throw CoreError("bind()", errno, strerror(errno));
-            }
-            if (listen(socket_fd, SOMAXCONN) < 0)
-            {
-                throw CoreError("listen()", errno, strerror(errno));
-            }
-            listen_sockets.insert(std::make_pair(config.pairs[i], socket_fd));
-            // std::cout << "Listening on " << config.pairs[i].first
-            //           << ":" << config.pairs[i].second
-            //           << " (fd=" << socket_fd << ")" << std::endl;
-            logListening(config.pairs[i].first, config.pairs[i].second);
+            skipped.insert(config.pairs[i]);
+            continue;
         }
+
+        socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (socket_fd < 0)
+            throw CoreError("socket()", errno, strerror(errno));
+
+        int opt_val = 1;
+        if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof(opt_val)) < 0)
+            throw CoreError("setsockopt()", errno, strerror(errno));
+
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        addr.sin_addr.s_addr = inet_addr(ip.c_str());
+
+        if (bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+            throw CoreError("bind()", errno, strerror(errno));
+
+        if (listen(socket_fd, SOMAXCONN) < 0)
+            throw CoreError("listen()", errno, strerror(errno));
+
+        listen_sockets.insert(std::make_pair(config.pairs[i], socket_fd));
+        logListening(ip, port);
     }
 }
+
 
 const Server &ServerCore::findserver(std::string ip, int port)
 {
